025dea3670d7010202340f2bf29fd2eb707c239a
mattirwin <mattirwin.socal@gmail.com>
2018-10-12 17:13:37 -0700

    expose and use ng2-mmbreakpoints in outer test project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Ng2MmbreakpointsService } from 'ng2-mmbreakpoints';
+import { mmCreate } from './mm-create';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{ provide: Ng2MmbreakpointsService, useFactory: mmCreate}]
 })
 export class AppComponent {
   title = 'ng2-mmbreakpoints-library';
+  displayBreakpoint: string;
+  displayColor: string;
+
+  constructor(private nn2: Ng2MmbreakpointsService, private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.updateMMData();
+    this.nn2.matchmediaChanges.subscribe(() => {
+      this.ngZone.run(() => {
+        this.updateMMData();
+      });
+    });
+  }
+
+  updateMMData() {
+    if (this.nn2.matchmediaCollection.xs.matches) {
+      this.displayBreakpoint = 'XS';
+      this.displayColor = 'blue';
+    } else if (this.nn2.matchmediaCollection.sm.matches) {
+      this.displayBreakpoint = 'SM';
+      this.displayColor = 'green';
+    } else if (this.nn2.matchmediaCollection.md.matches) {
+      this.displayBreakpoint = 'MD';
+      this.displayColor = 'gold';
+    } else if (this.nn2.matchmediaCollection.lg.matches) {
+      this.displayBreakpoint = 'LG';
+      this.displayColor = 'orange';
+    }
+  }
 }
